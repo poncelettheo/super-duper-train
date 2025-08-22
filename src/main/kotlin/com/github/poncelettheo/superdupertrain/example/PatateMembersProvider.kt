43320f400b9db059ae8f30cc217fd8347f2acf85
theopo <theo.po@hellowatt.fr>
2025-08-22 16:24:15 +0200

    We succeeded to detect nullable fields

--- src/main/kotlin/com/github/poncelettheo/superdupertrain/example/PatateMembersProvider.kt
+++ src/main/kotlin/com/github/poncelettheo/superdupertrain/example/PatateMembersProvider.kt
@@ -3,9 +3,15 @@ package com.github.poncelettheo.superdupertrain.example
 import com.intellij.psi.PsiElement
 import com.jetbrains.python.psi.types.PyClassType
 import com.jetbrains.python.codeInsight.PyCustomMember
+import com.jetbrains.python.psi.PyBoolLiteralExpression
+import com.jetbrains.python.psi.PyCallExpression
 import com.jetbrains.python.psi.PyClass
+import com.jetbrains.python.psi.PyExpression
+import com.jetbrains.python.psi.PyKeywordArgument
+import com.jetbrains.python.psi.PyTargetExpression
 import com.jetbrains.python.psi.types.PyClassMembersProviderBase
 import com.jetbrains.python.psi.types.TypeEvalContext
+import kotlin.math.exp
 
 class PatateMembersProvider : PyClassMembersProviderBase() {
     override fun getMembers(
@@ -17,7 +23,33 @@ class PatateMembersProvider : PyClassMembersProviderBase() {
 
         if (!isDjangoModel(pyClass, context)) return super.getMembers(clazz, location, context)
 
+        if (pyClass.qualifiedName == "blog.models.article.Article") {
+            pyClass.classAttributes.filter {isNullableDjangoField(it,context)}
+        }
+
+
         return listOf(PyCustomMember("patate"))
+    }
+
+    private fun isNullableDjangoField(expression: PyTargetExpression, context: TypeEvalContext): Boolean {
+        val call = expression.findAssignedValue() as? PyCallExpression ?: return false
+        if (!isDjangoFieldCall(call,context)) return false
+        if (!isNullable(call)) return false
+        return true
+    }
+
+    private fun isNullable(call: PyCallExpression): Boolean {
+        val value = call.getKeywordArgument("null") ?: return false
+        return (value as? PyBoolLiteralExpression)?.text == "True"
+    }
+
+    private fun isDjangoFieldCall(call: PyCallExpression, context: TypeEvalContext): Boolean {
+        val callee = call.callee ?: return false
+        val calleeClass = callee.reference?.resolve() as? PyClass ?: return false
+
+        return (calleeClass.isSubclass("django.db.models.Field", context)
+            || calleeClass.isSubclass("django.db.models.fields.Field", context))
+
     }
 
 
